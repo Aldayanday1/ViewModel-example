378939fb075c157c54e88f60a13d656392c68231
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-10-20 20:06:48 +0700

    Initial commit

---
+++ app/src/main/java/project/viewmodel/pertemuan5/data/dataform.kt
@@ -0,0 +1,5 @@
+package project.viewmodel.pertemuan5.data
+
+data class dataform(
+    val sex: String = ""
+)
---
+++ app/src/main/java/project/viewmodel/pertemuan5/data/datasource.kt
@@ -0,0 +1,10 @@
+package project.viewmodel.pertemuan5.data
+
+import project.viewmodel.pertemuan5.R
+
+object datasource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
